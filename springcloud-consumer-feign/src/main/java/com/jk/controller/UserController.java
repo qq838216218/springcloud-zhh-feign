e45e9cc990b261d6b1267c172f4b579f48ca3dd4
qq838216218 <838216218@qq.com>
2019-11-15 20:09:26 +0800

    update test123

--- springcloud-consumer-feign/src/main/java/com/jk/controller/UserController.java
+++ springcloud-consumer-feign/src/main/java/com/jk/controller/UserController.java
@@ -68,7 +68,7 @@ public class UserController {
     }
 
     public static void main(String[] args) {
-        System.out.println("test123 update");
+        System.out.println("test123 update!!!!!!!!!!!!!!!!!!!");
     }
 
 }
